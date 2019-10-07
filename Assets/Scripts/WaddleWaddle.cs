d2e162887c3dab4dc2c8bc2ca1cfb70807b613db
gonzalt <37196222+gonzalt@users.noreply.github.com>
2019-10-07 16:46:34 -0400

    Step 7: Placed Penguin Model and Plane

---
+++ Assets/Scripts/WaddleWaddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaddleWaddle : MonoBehaviour
+{
+	private Vector3 pos1 = new Vector3(-3, 0, -5);
+	private Vector3 pos2 = new Vector3(4, 0, -5);
+	public float speed = 1.0f;
+
+	private Vector3 rot1 = new Vector3(0, -235, 0);
+	private Vector3 rot2 = new Vector3(0, 235, 0);
+	public float rotSpeed = 1.0f;
+
+
+	void Update()
+	{
+
+
+		transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+
+
+	}
+
+
+}
